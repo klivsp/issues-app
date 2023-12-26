065e1ee493cdc3a5f2b83565a542f57398818706
Kliv Spahiu <102724470+KlivSpahiu@users.noreply.github.com>
2023-12-26 02:19:17 +0100

    Added new library for icons , modified navbar structure

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,17 +1,32 @@
 import Link from "next/link";
 import React from "react";
+import { AiFillBug } from "react-icons/ai";
 
 const NavBar = () => {
+  const links = [
+    {
+      name: "Dashboard",
+      url: "/",
+    },
+    {
+      name: "Issues",
+      url: "/issues",
+    },
+  ];
+
   return (
-    <div className="flex w-full border justify-between px-8">
-      <Link href="/">Logo</Link>
+    <div className="flex w-full border justify-between px-8 items-center">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
       <ul className="flex gap-4">
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/issues">Issues</Link>
-        </li>
+        {links.map((link, key) => {
+          return (
+            <li key={key}>
+              <Link href={link.url}>{link.name}</Link>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
